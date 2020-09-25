5497a67fced0bba9fb0c1e7908cbf5b472118b01
developer2 <mazzachris.work+developer2@gmail.com>
2020-09-24 21:19:24 -0500

    Initial Project Add

---
+++ src/main/java/mine/mazzanet/sfgpetclinictwo/SfgPetClinicTwoApplication.java
@@ -0,0 +1,13 @@
+package mine.mazzanet.sfgpetclinictwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mine/mazzanet/sfgpetclinictwo/SfgPetClinicTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package mine.mazzanet.sfgpetclinictwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
